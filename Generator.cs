82fc47e1525558986b6a1eb1e54afff1d9d7cf21
h4cksaw32 <yanshengm2019@gmail.com>
2025-05-25 20:27:36 -0700

    Add project files.

---
+++ Generator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DunGen.NET
+{
+    public class Generator
+    {
+        public required Grid grid;
+        private readonly Random rng = new();
+        public required Value2D<byte> TopLeftBound;
+        public required Value2D<byte> BottomRightBound;
+        public Value2D<byte> MapSegments = new Value2D<byte>{ x = 5, y = 5 };
+
+        public Value2D<byte> MinRoomSize = new Value2D<byte> { x = 3, y = 3 };
+        public Value2D<byte> MaxRoomSize = new Value2D<byte> { x = 16, y = 16 };
+        public bool OverlapRooms = false;
+        public byte RoomsPerSegment = 1;
+        public byte MinRoomExits = 1;
+        public byte MaxRoomExits = 4;
+
+        public float PoolJaggedness = 0.5F;
+        public bool OverlapPools = false;
+        public byte PoolsPerSegment = 1;
+
+        public float PathBend = 0.15F;
+        public float PathTerminate = 0.05F;
+        public bool Crossroads = true;
+        public bool EndAtBoundary = true;
+
+        public byte WallID = 0;
+        public required PoolOptions[] PoolIDs;
+        public required GroundOptions[] GroundIDs;
+    }
+    public struct PoolOptions
+    {
+        public byte id; 
+        public float spawnRate;
+    }
+    public struct GroundOptions
+    {
+        public byte id;
+        public float spawnRate;
+        public bool patched;
+        public Value2D<byte> MaxPatchSize;
+    }
+    public struct Value2D<T>
+    {
+        public T x;
+        public T y;
+    }
+}
---
+++ Grid.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace DunGen.NET
+{
+    public class Grid
+    {
+        private byte[] tiles;
+        public readonly byte width;
+        public readonly byte height;
+        public Grid(byte w, byte h, byte defValue = 0)
+        {
+            width = w;
+            height = h;
+            tiles = new byte[width * height];
+            for (int y = 0; y < height; y++)
+            {
+                for(int x = 0; x < width; x++)
+                {
+                    PlaceTile(x, y, defValue);
+                }
+            }
+        }
+        public byte GetTile(int x, int y) => tiles[y * width + x];
+        public void PlaceTile(int x, int y, byte value = 0)
+        {
+            tiles[y * width + x] = value;
+        }
+        public void InsertTile(int x, int y, byte value = 0)
+        {
+            for (int i = y * (width + 1) - 1; i > y * width + x; i--)
+            {
+                tiles[i] = tiles[i - 1];
+            }
+            PlaceTile(x, y, value);
+        }
+        public byte?[,] GetArea(int x, int y)
+        {
+            byte?[,] result = new byte?[3, 3];
+            result[1, 1] = GetTile(x, y);
+            result[0, 0] = x > 0 && y > 0 ? GetTile(x - 1, y - 1) : null;
+            result[0, 1] = y > 0 ? GetTile(x, y - 1) : null;
+            result[0, 2] = x < width - 1 && y > 0 ? GetTile(x + 1, y - 1) : null;
+            result[1, 0] = x > 0 ? GetTile(x - 1, y) : null;
+            result[1, 2] = x < width - 1 ? GetTile(x + 1, y) : null;
+            result[2, 0] = x > 0 && y < height - 1 ? GetTile(x - 1, y + 1) : null;
+            result[2, 1] = y < height - 1 ? GetTile(x, y + 1) : null;
+            result[2, 0] = x < width - 1 && y < height - 1 ? GetTile(x + 1, y + 1) : null;
+            return result;
+        }
+    }
+}
