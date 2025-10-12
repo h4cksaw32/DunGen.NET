764df5a188b4a9e18dd82c9c1b39a4d3920b58c0
h4cksaw <yanshengm2019@gmail.com>
2025-10-12 14:55:33 -0700

    Undid all the assembly shenanigans

--- DunGenApp/Program.cs
+++ DunGenApp/Program.cs
@@ -1,7 +1,5 @@
 ﻿using Avalonia;
 using System;
-using System.IO;
-using System.Reflection;
 
 namespace DunGenApp
 {
@@ -11,21 +9,9 @@ namespace DunGenApp
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
         [STAThread]
-        public static void Main(string[] args)
-        {
-            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(AssemblyResolve);
-            Start(args);
-        }
-        internal static void Start(string[] args) =>
+        public static void Main(string[] args) =>
             BuildAvaloniaApp()
             .StartWithClassicDesktopLifetime(args);
-        private static Assembly AssemblyResolve(object? sender, ResolveEventArgs args)
-        {
-            string defDir = args.RequestingAssembly.GetName().Name + ".dll";
-            string altDir = Path.Combine("lib", args.RequestingAssembly.GetName().Name + ".dll");
-            Assembly asm = Assembly.LoadFrom(File.Exists(defDir) ? defDir : altDir);
-            return asm;
-        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
